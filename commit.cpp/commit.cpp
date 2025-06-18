8fdc47a761f7cee1457bc48e76166477e269fe9e
Yadeni Getu <yadeni.ugr-7202-16@aau.edu.et>
2025-06-19 00:00:54 +0300

    final for commit.cpp

--- commit.cpp/commit.cpp
+++ commit.cpp/commit.cpp
@@ -30,27 +30,3 @@ void commit(const string& message) {
     ofstream(INDEX_FILE);
     cout << "Committed. Hash: " << commitHash << endl;
 }
-
-., [6/18/2025 9:47 PM]
-void log() {
-    string current = getBranchCommit(getHeadBranch());
-    while (current != "null") {
-        ifstream in(COMMITS_DIR + "/" + current);
-        string line;
-        cout << "Commit: " << current << endl;
-        while (getline(in, line)) {
-            if (line.rfind("msg:", 0) == 0  line.rfind("time:", 0) == 0  line.rfind("parent:", 0) == 0)
-                cout << line << endl;
-        }
-        cout << "-----------------------\n";
-
-        in.clear();
-        in.seekg(0);
-        while (getline(in, line)) {
-            if (line.rfind("parent:", 0) == 0) {
-                current = line.substr(7);
-                break;
-            }
-        }
-    }
-}
